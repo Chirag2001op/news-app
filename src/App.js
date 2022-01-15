239c9f30a0e56f983b6338ddfcb95e14a3cfa429
Chirag2001op <chirag.dogra79@gmail.com>
2022-01-15 16:16:21 +0530

    empty-dom-2 solved

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/navbar';
 import Newses from './components/newses';
 import {
   BrowserRouter as Router,
-  Routes,
+  Switch,
   Route
 } from "react-router-dom";
 
@@ -12,15 +12,15 @@ function App() {
    <div>
      <Router>
     <Navbar/>
-    <Routes>
-      <Route exact path="/" ><Newses key="general" pageSize={6} country ="in" category="general"/></Route>
-      <Route exact path="/business" ><Newses key="business" pageSize={6} country ="in" category="business"/></Route>
-      <Route exact path="/entertainment" ><Newses key="entertainment" pageSize={6} country ="in" category="entertainment"/></Route>
-      <Route exact path="/health" ><Newses key="health" pageSize={6} country ="in" category="health"/></Route>
-      <Route exact path="/science" ><Newses key="science" pageSize={6} country ="in" category="science"/></Route>
-      <Route exact path="/sports" ><Newses key="sports" pageSize={6} country ="in" category="sports"/></Route>
-      <Route exact path="/technology" ><Newses key="technology" pageSize={6} country ="in" category="technology"/></Route>
-    </Routes>
+    <Switch>
+      <Route  path="/" ><Newses key="general" pageSize={6} country ="in" category="general"/></Route>
+      <Route  path="/business" ><Newses key="business" pageSize={6} country ="in" category="business"/></Route>
+      <Route  path="/entertainment" ><Newses key="entertainment" pageSize={6} country ="in" category="entertainment"/></Route>
+      <Route  path="/health" ><Newses key="health" pageSize={6} country ="in" category="health"/></Route>
+      <Route  path="/science" ><Newses key="science" pageSize={6} country ="in" category="science"/></Route>
+      <Route  path="/sports" ><Newses key="sports" pageSize={6} country ="in" category="sports"/></Route>
+      <Route  path="/technology" ><Newses key="technology" pageSize={6} country ="in" category="technology"/></Route>
+    </Switch>
    
     </Router>
    </div>
